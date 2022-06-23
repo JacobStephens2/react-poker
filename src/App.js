fbbf2aac3353815d6eecbb6b02bcaf38c37091ee
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-06-22 22:35:13 -0400

    add community card section

--- src/App.js
+++ src/App.js
@@ -9,27 +9,34 @@ function App() {
   const [cardsP2, setCardsP2] = useState([]);
   const [deck, setDeck] = useState(FrenchDeck);
   const [communityCards, setCommunityCards] = useState([]);
-  const [playerTurn, setPlayerTurn] = useState(1);
   const [pot, setPot] = useState(0);
 
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min);
+    // The maximum and minimum are both inclusive
   }
 
   function P1DrawOneCard() {
     let cardDrawn = getRandomIntInclusive(0, deck.length);
     let newCard = deck[cardDrawn];
-    setCardsP1(deck => [...deck, newCard]);
     setDeck(deck.filter(deck => deck.id != newCard.id))
+    setCardsP1(deck => [...deck, newCard]);
   }
 
   function P2DrawOneCard() {
     let cardDrawn = getRandomIntInclusive(0, deck.length);
     let newCard = deck[cardDrawn];
-    setCardsP2(deck => [...deck, newCard]);
     setDeck(deck.filter(deck => deck.id != newCard.id))
+    setCardsP2(deck => [...deck, newCard]);
+  }
+
+  function drawCommunityCard() {
+    let cardDrawn = getRandomIntInclusive(0, deck.length);
+    let newCard = deck[cardDrawn];
+    setDeck(deck.filter(deck => deck.id != newCard.id))
+    setCommunityCards(deck => [...deck, newCard]);
   }
 
   function P1Bet() {
@@ -45,12 +52,25 @@ function App() {
 
   return (
     <div className="App">
+
       <h1>Poker</h1>
-      <div>Player {playerTurn}'s Turn</div>
-      <div>Community Cards: {communityCards}</div>
+
+      <div>{communityCards.length} Community Cards</div>
+      <button onClick={drawCommunityCard}>Draw One Card</button>
+      <ul>{
+        communityCards.map((card) =>
+          <li key={card.id}>
+            {card.value} of {card.suit}
+          </li>
+        )
+      }</ul>
       <div>Pot: ${pot}</div>
+
       <h2>Player 1</h2>
       <div>Chips: ${chipsP1}</div>
+      <button onClick={P1DrawOneCard}>Draw One Card</button>
+      <button onClick={P1Bet}>Bet $1</button>
+      <div>{cardsP1.length} Card Hand</div>
       <ul>{
         cardsP1.map((card) =>
           <li key={card.id}>
@@ -58,11 +78,12 @@ function App() {
           </li>
         )
       }</ul>
-      <button onClick={P1DrawOneCard}>Draw One Card</button>
-      <button onClick={P1Bet}>Bet</button>
+
       <h2>Player 2</h2>
       <div>Chips: ${chipsP2}</div>
-      <div>Cards:</div>
+      <button onClick={P2DrawOneCard}>Draw One Card</button>
+      <button onClick={P2Bet}>Bet $1</button>
+      <div>{cardsP2.length} Card Hand</div>
       <ul>{
         cardsP2.map((card) =>
           <li key={card.id}>
@@ -70,16 +91,15 @@ function App() {
           </li>
         )
       }</ul>
-      <button onClick={P2DrawOneCard}>Draw One Card</button>
-      <button onClick={P2Bet}>Bet</button>
-      <h2>Deck of {deck.length}</h2>
-      <ol>{
+
+      <h2>{deck.length} Card Deck</h2>
+      <ul>{
         deck.map((card) =>
           <li key={card.id}>
             {card.value} of {card.suit}
           </li>
         )
-      }</ol>
+      }</ul>
     </div>
   );
 }
